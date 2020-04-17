76f65f334345c7837aeb60f5ee9bde8266ba497e
unknown <pawel.jadach.96@gmail.com>
2020-04-17 13:13:50 +0200

    Add bottomNavBar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,24 +3,46 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_reduxx/components/drawers/DrawerAdmin.dart';
 import 'package:flutter_reduxx/components/drawers/DrawerAnonymous.dart';
 import 'package:flutter_reduxx/components/drawers/DrawerUser.dart';
-import 'package:flutter_reduxx/components/filmList.dart';
+import 'Shows/showList.dart';
 import 'package:flutter_reduxx/models/film.dart';
 import 'package:flutter_reduxx/models/role.dart';
 import 'package:flutter_reduxx/models/showing.dart';
 import 'package:flutter_reduxx/models/user.dart';
 import 'package:flutter_reduxx/redux/app_state.dart';
+import 'package:flutter_reduxx/views/Price/price.dart';
 import 'package:flutter_reduxx/views/home_viewmodel.dart';
 import 'package:redux/redux.dart';
 
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
 
   final Store<AppState> store;
 
   Home(this.store);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-final List<Showing> showing = [
+class _HomeState extends State<Home> {
+int _selectedIndex = 0;
+
+List<Widget> _widgetOptions = <Widget>[
+
+  Price(),
+  ShowingList(showing: showing),
+  Price(),
+  Price(),
+
+];
+
+void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+static final List<Showing> showing = [
   Showing(film: Film(img: 'https://bi.im-g.pl/im/15/b7/15/z22769941Q,Shrek.jpg', title: 'Shrek', timeInMinutes: 120, minAge: 3), hour: '12:00', date: '20 października 2020'),
   Showing(film: Film(img: 'https://techsetter.pl/wp-content/uploads/2019/12/rise_kubacki_main1.jpg', title: 'Star Wars', timeInMinutes: 170, minAge: 13), hour: '21:00', date: '21 października 2020'),
   Showing(film: Film(img: 'https://i.ytimg.com/vi/OqMN-lInSXw/hqdefault.jpg', title: 'Za szybcy za wśiekli', timeInMinutes: 120, minAge: 16), hour: '12:00', date: '20 października 2020'),
@@ -33,6 +55,10 @@ final List<Showing> showing = [
 ];
 
 
+static final List<Film> films = [
+  Film(img: 'https://bi.im-g.pl/im/15/b7/15/z22769941Q,Shrek.jpg', title: 'Shrek', timeInMinutes: 120, minAge: 3),
+  Film(img: 'https://techsetter.pl/wp-content/uploads/2019/12/rise_kubacki_main1.jpg', title: 'Star Wars', timeInMinutes: 170, minAge: 13),
+];
 
 
   @override
@@ -47,7 +73,7 @@ final List<Showing> showing = [
           return DrawerAdmin();
           break;
         case Role.USER:
-          return DrawerUser(store);
+          return DrawerUser(widget.store);
           break;
         default: return DrawerAnonymous();
       }
@@ -61,7 +87,39 @@ final List<Showing> showing = [
         ),
         drawer: takeRole(viewModel.user),
         body: Center(
-          child: FilmList(showing: showing),
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.redAccent,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.black),
+              activeIcon: Icon(Icons.home, color: Colors.red),
+              title: Text('Nowości'),
+              // backgroundColor: Colors.black,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business, color: Colors.black),
+              activeIcon: Icon(Icons.business, color: Colors.red),
+              title: Text('Seanse'),
+              // backgroundColor: Colors.black,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school, color: Colors.black),
+              activeIcon: Icon(Icons.school, color: Colors.red),
+              title: Text('Filmy'),
+              // backgroundColor: Colors.black,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school, color: Colors.black),
+              activeIcon: Icon(Icons.school, color: Colors.red),
+              title: Text('Cennik'),
+              // backgroundColor: Colors.black,
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.red[300],
+          onTap: _onItemTapped,
         ),
       )
     );
